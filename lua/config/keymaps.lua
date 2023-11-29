cc5e998db57cd9ef4f3b846972609d288651cd1f
Eric Lin <tttttt123465@gmail.com>
2023-11-29 20:38:57 +0800

    add plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,23 @@ keymap.set("n", "<C-j>", function()
   keymap.set("n", "<leader>rn", ":IncRename ")
   vim.diagnostic.goto_next()
 end, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fq", builtin.quickfix, {})
+
+vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
+  desc = "Toggle Spectre",
+})
+vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+  desc = "Search current word",
+})
+vim.keymap.set("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+  desc = "Search current word",
+})
+vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+  desc = "Search on current file",
+})
